e316bc8fcee1d1aad1945f0a450b31f415bf9e45
MariusKlug <marius.s.klug@gmail.com>
2021-10-12 16:55:38 +0200

    add first assignment

---
+++ Assignments/assignment_1/assignment_1.m
@@ -0,0 +1,95 @@
+% Assignment 1 for the class Data Science for Human Factors:
+% MATLAB is a very fancy calculator
+% 
+% Write the code in the sections below and, if necessary, your final answer
+% as a comment.
+%
+% The filename should be assignment_1_team_<x>
+%
+% Team: 
+
+scores = 0;
+
+%% 1) (1.5P)
+% MATLAB as a Calculator
+
+% a) What is larger: The square of 1.5 or the third root of the square of 3? 
+% Compute this here, leave the code in, and delete the wrong answer. (0.25P)
+
+% -> The square of 1.5 / The third root of the square of 3
+
+% b) An airplane flies with an average speed of 800km/h from Berlin to New York.
+% The distance is 6381km. How long will it take to get there? 
+% Give your answers in hours:min:sec. (0.5P)
+
+
+% It takes the airplane h:mm:ss to fly to New York.
+
+% c) How long does it take the airplane to fly once around the earth? 
+% (Hint: google "MATLAB earth radius", "MATLAB pi")
+% Use variables in your code!
+% Give your answers in hours:min:sec. (0.75P)
+
+
+% It takes the airplane hh:mm:ss to fly around the earth.
+
+%%
+scores(1) = 0
+
+%% 2) (2P)
+% Use the colon operator to obtain the following number sequences
+
+% a) 1 2 3 4 5 (0.2P)
+
+
+% b) -4.0000   -3.5000   -3.0000   -2.5000   -2.0000 (0.2P)
+
+
+% c) 10 20 30 40 50 60 70 (0.2P)
+
+
+% d) -4 15 34 53 (0.2P)
+
+
+% e) 100 91 82 73 64 55 46 (0.2P)
+
+
+% f) "1:10" obtains the integer numbers from 1 to 10. 
+% Add exactly two characters to the command to obtain only odd numbers. (0.5P)
+
+
+% g) What can be changed in the command that will not lead to a different result? (0.5P)
+%       Don't just recompute the input, but change the content of the
+%       command (e.g. NOT 1:(1+1):10)
+
+
+%%
+scores(2) = 0
+
+%% 3) (1P)
+% Working with variables
+
+% a) Create code that calculates the area of a circle for a variable radius r. (0.5P)
+
+% e.g.
+r = 5;
+
+area = 
+
+% b) Create code that caculates the quadratic formula (http://bfy.tw/30jL)
+% for the variables a, b, and c. (0.5P)
+
+% e.g.
+a = 2;
+b = -4;
+c = -16;
+
+x_1 = 
+x_2 = 
+
+%% 
+scores(3) = 0
+
+%%
+final_score = sum(scores)
+
